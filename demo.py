1673992cfe47a572e487c5110eb530f24ca9a82b
huanghuajin <huanghuajin@2144.cn>
2017-12-29 15:00:04 +0800

    add demo.py

---
+++ demo.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import time
+import json
+
+output = [{"endpoint": "monitor-test-centos", "tags": "", "timestamp": int(time.time()), "metric": "agent.cpu", "value": 1.8, "counterType": "GAUGE", "step": 60}]
+print json.dumps(output)
